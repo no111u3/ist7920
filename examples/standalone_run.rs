a542f1f16eda6694ec250d5778cc78a9248c6e75
Boris Vinogradov <no111u3@gmail.com>
2021-06-26 17:48:06 +0300

    Add draft implementation of init and draw display and example for pure draw

---
+++ examples/standalone_run.rs
@@ -0,0 +1,106 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+
+use cortex_m_rt::entry;
+
+use cortex_m::peripheral::Peripherals;
+
+use stm32f4xx_hal::{
+    delay::Delay,
+    prelude::*,
+    spi::{self, Spi},
+    stm32,
+};
+
+use display_interface_spi::SPIInterface;
+use ist7920::Ist7920;
+
+#[entry]
+fn main() -> ! {
+    let p = stm32::Peripherals::take().unwrap();
+
+    let cp = Peripherals::take().unwrap();
+
+    let gpioa = p.GPIOA.split();
+    let gpiob = p.GPIOB.split();
+    let rcc = p.RCC.constrain();
+
+    let clocks = rcc.cfgr.freeze();
+
+    let mut led = gpioa.pa5.into_push_pull_output();
+    led.set_low().ok();
+
+    let sck = gpiob.pb3.into_alternate_af5();
+    let miso = spi::NoMiso;
+    let mosi = gpiob.pb5.into_alternate_af5();
+
+    let dc = gpiob.pb4.into_push_pull_output();
+    let mut res = gpiob.pb10.into_push_pull_output();
+    let cs = gpiob.pb13.into_push_pull_output();
+
+    let mut delay = Delay::new(cp.SYST, clocks);
+
+    let mode = spi::Mode {
+        polarity: spi::Polarity::IdleLow,
+        phase: spi::Phase::CaptureOnFirstTransition,
+    };
+
+    let spi = Spi::spi1(p.SPI1, (sck, miso, mosi), mode, 8_000_000.hz(), clocks);
+
+    let iface = SPIInterface::new(spi, dc, cs);
+
+    let mut display = Ist7920::new(iface);
+
+    display.reset(&mut res, &mut delay);
+
+    display.init(&mut delay);
+
+    /*display.set_draw_area((190, 78), (190 + 83, 78 + 23));
+    for _ in 0..(54 * 44) {
+        display.draw(&[0x00]);
+    }
+    display.set_draw_area((192, 80), (192 + 79, 80 + 19));
+    for i in 0..(50 * 40) {
+        display.draw(&[(i % 4) as u8 | (((i >> 2) % 4) << 2) as u8]);
+    }*/
+
+    for i in 0..(128 * 128) {
+        display.draw(&[i as u8]);
+    }
+
+    //let mut select_figure = 0;
+    loop {
+        delay.delay_ms(500_u16);
+        /*let (begin, end) = match select_figure {
+            0 => {
+                select_figure = 1;
+                ((10, 10), (89, 89))
+            }
+
+            1 => {
+                select_figure = 2;
+                ((100, 10), (100 + 79, 89))
+            }
+            _ => {
+                select_figure = 0;
+                ((180 + 10, 10), (180 + 41, 41))
+            }
+        };
+        display.set_draw_area(begin, end);
+        for _ in 0..((end.1 - begin.1 + 1) / 16) {
+            for _ in 0..((end.0 - begin.0 + 1) / 2) {
+                display
+                    .draw(&[0x77, 0x77, 0x77, 0x77, 0xff, 0xff, 0xff, 0xff])
+                    .ok();
+            }
+            for _ in 0..((end.0 - begin.0 + 1) / 2) {
+                display
+                    .draw(&[0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00])
+                    .ok();
+            }
+        }*/
+        led.toggle().ok();
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,90 @@
 #![no_std]
 //! Generic SPI interface for display drivers
+pub mod error;
+
+use error::Error;
+
+use display_interface::{DataFormat::U8, DisplayError, WriteOnlyDataCommand};
+use display_interface_spi::SPIInterfaceNoCS;
+use embedded_hal::{blocking::delay::DelayMs, digital::v2::OutputPin};
+
+/// IST7920 LCD display driver.
+#[derive(Copy, Clone, Debug)]
+pub struct Ist7920<DI> {
+    interface: DI,
+}
+
+impl<DI> Ist7920<DI>
+where
+    DI: WriteOnlyDataCommand,
+{
+    /// Create a IST7920 interface
+    pub fn new(interface: DI) -> Self {
+        Self { interface }
+    }
+
+    /// Initialise the display in one of the available addressing modes.
+    /// TODO: Add address setup
+    pub fn init<DELAY>(&mut self, delay: &mut DELAY)
+    where
+        DELAY: DelayMs<u8>,
+    {
+        self.interface.send_commands(U8(&[0x76])); // Software reset.
+        delay.delay_ms(50);
+        self.interface.send_commands(U8(&[0x3c])); // Display Off
+        self.interface.send_commands(U8(&[0x90, 128])); // Set Duty
+        self.interface.send_commands(U8(&[0x30, 16])); // Set Bias
+        self.interface.send_commands(U8(&[0x31, 0x3f])); // Set voltage generate clock
+        self.interface.send_commands(U8(&[0x33, 0x20])); // Power control
+        delay.delay_ms(100);
+        self.interface.send_commands(U8(&[0x33, 0x2c])); // Power control
+        delay.delay_ms(100);
+        self.interface.send_commands(U8(&[0xfd])); // Set booster
+        delay.delay_ms(100);
+        self.interface.send_commands(U8(&[0x33, 0x2f])); // Power control
+        delay.delay_ms(200);
+        self.interface.send_commands(U8(&[0x064])); // Display Ctrl: Bit3: SHL 2:ADC 1:EON, 0:REV
+
+        self.interface.send_commands(U8(&[0x074, 0x000, 0x00f])); // AY Window
+        self.interface.send_commands(U8(&[0x075, 0x000, 0x07f])); // AX Window
+
+        self.interface.send_commands(U8(&[0x040, 64])); // Start line at 64
+
+        self.interface.send_commands(U8(&[0x0b1, 110])); // electronic volume
+
+        self.interface.send_commands(U8(&[0x3d])); // Display on
+    }
+
+    /// Send a raw buffer to the display.
+    pub fn draw(&mut self, buffer: &[u8]) -> Result<(), DisplayError> {
+        self.interface.send_data(U8(&buffer))
+    }
+
+    /// Reset the display.
+    pub fn reset<RST, DELAY, PinE>(
+        &mut self,
+        rst: &mut RST,
+        delay: &mut DELAY,
+    ) -> Result<(), Error<(), PinE>>
+    where
+        RST: OutputPin<Error = PinE>,
+        DELAY: DelayMs<u8>,
+    {
+        inner_reset(rst, delay)
+    }
+}
+
+fn inner_reset<RST, DELAY, PinE>(rst: &mut RST, delay: &mut DELAY) -> Result<(), Error<(), PinE>>
+where
+    RST: OutputPin<Error = PinE>,
+    DELAY: DelayMs<u8>,
+{
+    rst.set_high().map_err(Error::Pin)?;
+    delay.delay_ms(1);
+    rst.set_low().map_err(Error::Pin)?;
+    delay.delay_ms(10);
+    rst.set_high().map_err(Error::Pin)?;
+    delay.delay_ms(20);
+
+    Ok(())
+}
